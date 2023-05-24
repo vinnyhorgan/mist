e4191efcd9de020b519a1212289003b5ec586526
TonyDev <antoniospano05@outlook.it>
2023-05-24 11:22:43 +0200

    Added vscode to ignore and improved build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
+mkdir -p bin
 clang++ -std=c++14 -framework CoreVideo -framework IOKit -framework Cocoa -framework GLUT -framework OpenGL -I deps/include runtime/src/main.cpp deps/lib/libraylib.a -o bin/runtime
+./bin/runtime
