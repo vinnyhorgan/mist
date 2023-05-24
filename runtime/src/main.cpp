104012b0e70612c77218a90cf07a3f397c82c3d1
TonyDev <antoniospano05@outlook.it>
2023-05-24 11:58:28 +0200

    Added json serialization

--- runtime/src/main.cpp
+++ runtime/src/main.cpp
@@ -1,7 +1,34 @@
 #include <raylib.h>
+#include <fstream>
+#include <json.hpp>
+#include <iostream>
+
+using json = nlohmann::json;
 
 int main()
 {
+    std::ifstream f("test.json");
+    if (!f.is_open())
+    {
+        return 1;
+    }
+
+    json data = json::parse(f);
+
+    f.close();
+
+    if (data.find("entities") != data.end() && data["entities"].is_array())
+    {
+        for (const auto& entity : data["entities"])
+        {
+            std::cout << entity << std::endl;
+        }
+    }
+    else
+    {
+        return 1;
+    }
+
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(800, 600, "Mist Runtime");
     SetTargetFPS(60);
@@ -11,6 +38,15 @@ int main()
         BeginDrawing();
 
         ClearBackground(BLACK);
+
+        for (const auto& entity : data["entities"])
+        {
+            std::string name = entity.get<std::string>();
+
+            DrawText(name.c_str(), GetRandomValue(0, GetScreenWidth()), GetRandomValue(0, GetScreenHeight()), 50, WHITE);
+        }
+
+        DrawFPS(10, 10);
 
         EndDrawing();
     }
