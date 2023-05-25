15bfae4fbb314fe23573ce12fcd0d52c7f1cb563
Vinny Horgan <vinnyhorgan@gmail.com>
2023-05-25 13:37:24 +0200

    Started work on editor gui

--- editor/src/main.cpp
+++ editor/src/main.cpp
@@ -1,6 +1,11 @@
 #include "Logger.h"
 
 #include <raylib.h>
+#include <raymath.h>
+
+#define RAYGUI_IMPLEMENTATION
+#include <raygui.h>
+
 #include <fstream>
 #include <json.hpp>
 #include <iostream>
@@ -9,21 +14,49 @@ using json = nlohmann::json;
 
 int main()
 {
+    const int windowWidth = 1280;
+    const int windowHeight = 720;
+
     SetTraceLogCallback(Logger::rayLog);
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-    InitWindow(800, 600, "Mist Editor");
+    InitWindow(windowWidth, windowHeight, "Mist Editor - v0.0.1");
+    SetWindowMinSize(windowWidth, windowHeight);
     SetTargetFPS(60);
+
+    RenderTexture2D viewport = LoadRenderTexture(200, 200);
+    SetTextureFilter(viewport.texture, TEXTURE_FILTER_POINT);
 
     Logger::setDebug(true);
 
+    bool sceneWindowActive = true;
+    bool newEntityButtonPressed = false;
+
     while (!WindowShouldClose())
     {
+        BeginTextureMode(viewport);
+
+            ClearBackground(SKYBLUE);
+
+        EndTextureMode();
+
         BeginDrawing();
 
-        ClearBackground(BLACK);
+            ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
+
+            if (sceneWindowActive)
+            {
+                sceneWindowActive = !GuiWindowBox((Rectangle){ 0, 0, 192, GetScreenHeight() }, "SCENE");
+                newEntityButtonPressed = GuiButton((Rectangle){ 24, 48, 144, 24 }, "NEW ENTITY");
+                GuiLine((Rectangle){ 24, 88, 144, 16 }, NULL);
+                GuiLabel((Rectangle){ 24, 120, 144, 24 }, "ENTITIES");
+            }
+
+            DrawTexture(viewport.texture, 200, 200, WHITE);
 
         EndDrawing();
     }
+
+    UnloadRenderTexture(viewport);
 
     CloseWindow();
 
--- runtime/src/main.cpp
+++ runtime/src/main.cpp
@@ -1,9 +1,13 @@
 #include "Logger.h"
 
 #include <raylib.h>
+#include <raymath.h>
 #include <fstream>
 #include <json.hpp>
 #include <iostream>
+
+#define MAX(a, b) ((a)>(b)? (a) : (b))
+#define MIN(a, b) ((a)<(b)? (a) : (b))
 
 using json = nlohmann::json;
 
@@ -33,30 +37,55 @@ int main()
         return 1;
     }
 
+    const int windowWidth = 1280;
+    const int windowHeight = 720;
+
     SetTraceLogCallback(Logger::rayLog);
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-    InitWindow(800, 600, "Mist Runtime");
+    InitWindow(windowWidth, windowHeight, "Mist Runtime - v0.0.1");
+    SetWindowMinSize(windowWidth, windowHeight);
     SetTargetFPS(60);
+
+    RenderTexture2D target = LoadRenderTexture(windowWidth, windowHeight);
+    SetTextureFilter(target.texture, TEXTURE_FILTER_POINT);
 
     Logger::setDebug(true);
 
     while (!WindowShouldClose())
     {
+        float scale = MIN((float)GetScreenWidth() / windowWidth, (float)GetScreenHeight() / windowHeight);
+
+        Vector2 mouse = GetMousePosition();
+        Vector2 virtualMouse = { 0 };
+        virtualMouse.x = (mouse.x - (GetScreenWidth() - (windowWidth * scale)) * 0.5f) / scale;
+        virtualMouse.y = (mouse.y - (GetScreenHeight() - (windowHeight * scale)) * 0.5f) / scale;
+        virtualMouse = Vector2Clamp(virtualMouse, (Vector2){ 0, 0 }, (Vector2){ (float)windowWidth, (float)windowHeight });
+
+        BeginTextureMode(target);
+
+            ClearBackground(SKYBLUE);
+
+            for (const auto& entity : data["entities"])
+            {
+                std::string name = entity.get<std::string>();
+
+                DrawText(name.c_str(), GetRandomValue(0, GetScreenWidth()), GetRandomValue(0, GetScreenHeight()), 50, WHITE);
+            }
+
+        EndTextureMode();
+
         BeginDrawing();
 
-        ClearBackground(BLACK);
+            ClearBackground(BLACK);
 
-        for (const auto& entity : data["entities"])
-        {
-            std::string name = entity.get<std::string>();
-
-            DrawText(name.c_str(), GetRandomValue(0, GetScreenWidth()), GetRandomValue(0, GetScreenHeight()), 50, WHITE);
-        }
-
-        DrawFPS(10, 10);
+            DrawTexturePro(target.texture, (Rectangle){ 0.0f, 0.0f, (float)target.texture.width, (float)-target.texture.height },
+                           (Rectangle){ (GetScreenWidth() - ((float)windowWidth * scale)) * 0.5f, (GetScreenHeight() - ((float)windowHeight * scale)) * 0.5f,
+                           (float)windowWidth * scale, (float)windowHeight * scale }, (Vector2){ 0, 0 }, 0.0f, WHITE);
 
         EndDrawing();
     }
+
+    UnloadRenderTexture(target);
 
     CloseWindow();
 
